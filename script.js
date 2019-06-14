73aa825dae0f96e4f2fbf4fb94e4cfe5fd3bf378
Jonathan <jonathan.willian3443@gmail.com>
2019-06-14 17:04:46 -0300

    Site copia da copia2

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let idade = prompt("Qual sua idade?");
+let pesquisa = document.querySelector("#search input");
+let butao = document.querySelector("#search img");
+
+
 let filmes_cartaz = [
-  {titulo:"Harry Potter", idade: 10},
-  {titulo:"It, a coisa", idade: 16},
+  {titulo:["Vingadores: Ultimato","Vingadores: Guerra Infinita"], genero: "ação", elenco: ["Paul Walker",""]},
+  {titulo:["Velozes e Furiosos 1","Velozes e furiosos 2"], genero: "corridas", elenco:},
   {titulo:"O albergue", idade: 18}
+]
+
+
+function pesquisar(){
+    if()
+}
+
+
+
 ]
 for(let i=0; i<filmes_cartaz.length; i++){
   if(idade)
