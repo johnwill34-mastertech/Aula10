46067519c505319ec0eec3ca9d69edcfb63a332b
Jonathan <jonathan.willian3443@gmail.com>
2019-06-14 16:13:20 -0300

    Site copia da copia

---
+++ script.js
@@ -0,0 +1,9 @@
+let idade = prompt("Qual sua idade?");
+let filmes_cartaz = [
+  {titulo:"Harry Potter", idade: 10},
+  {titulo:"It, a coisa", idade: 16},
+  {titulo:"O albergue", idade: 18}
+]
+for(let i=0; i<filmes_cartaz.length; i++){
+  if(idade)
+}
